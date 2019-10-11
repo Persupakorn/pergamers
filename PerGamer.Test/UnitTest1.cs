d4cfa72850d28ae695b5368bd24b6ab57c7d31c9
supakorn yungpayoon <per.supakorn@hotmail.com>
2019-10-11 19:10:13 +0700

    change

--- PerGamer.Test/UnitTest1.cs
+++ PerGamer.Test/UnitTest1.cs
@@ -8,17 +8,47 @@ namespace PerGamer.Test
     public class UnitTest1
     {
         HomeController data = new HomeController();
-        
+
         [Theory(DisplayName = "service 01 เลือกได้แค่ 01 หรือ 02")]
-        [InlineData("01",true)]
-        [InlineData("02", true)]
-        public void Service(string serviceid,bool issuccess)
+        [InlineData("01", "01", "Success")]
+        [InlineData("01", "02", "Success")]
+        [InlineData("01", "03", "Success")]
+        [InlineData("01", "04", "Success")]
+        [InlineData("01", "05", "Success")]
+        [InlineData("02", "06", "Success")]
+        [InlineData("02", "07", "Success")]
+        [InlineData("", "", "Error")]
+        [InlineData("", "01", "Error")]
+        [InlineData("", "02", "Error")]
+        [InlineData("", "03", "Error")]
+        [InlineData("", "04", "Error")]
+        [InlineData("", "05", "Error")]
+        [InlineData("", "06", "Error")]
+        [InlineData("", "07", "Error")]
+        [InlineData("01", "", "Error")]
+        [InlineData(null, "", "Error")]
+        [InlineData(null, "01", "Error")]
+        [InlineData(null, "02", "Error")]
+        [InlineData(null, "03", "Error")]
+        [InlineData(null, "04", "Error")]
+        [InlineData(null, "05", "Error")]
+        [InlineData(null, "06", "Error")]
+        [InlineData(null, "07", "Error")]
+        [InlineData("01", null, "Error")]
+        [InlineData("02", null, "Error")]
+        [InlineData(null, null, "Error")]
+        [InlineData("12341234", null, "Error")]
+        [InlineData("12341234", "12341324", "Error")]
+        public void SelectService(string serviceid, string objectId, string message)
         {
             var input = new ServiceInput()
             {
-                ServiceId = serviceid
+                ServiceId = serviceid,
+                ObjectId = objectId
             };
-            var test = data.tableSelectService(input);
+            var result = data.tableSelectService(input);
+            Assert.Equal(result, message);
         }
+
     }
 }
--- PerGamer/Controllers/HomeController.cs
+++ PerGamer/Controllers/HomeController.cs
@@ -27,9 +27,59 @@ namespace PerGamer.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public bool tableSelectService(ServiceInput input)
+        public string tableSelectService(ServiceInput input)
         {
-            throw new NotImplementedException();
+            string[] setServiceId = { "01", "02" };
+            string[] setObjectiveIdService01 = { "01", "02", "03", "04", "05" };
+            string[] setObjectiveIdService02 = { "06", "07" };
+            string[] setMethodIdObjective = { "01", "02" };
+            string[] setMethodIdObjectiveMethod01 = { "02" };
+            string[] setMethodIdObjectiveMethod02 = { "01", "02" };
+            if (string.IsNullOrEmpty(input.ServiceId))
+            {
+                return "Error";
+            }
+            else if (setServiceId.Any(input.ServiceId.Contains))
+            {
+                if (input.ServiceId == "01")
+                {
+                    if (string.IsNullOrEmpty(input.ObjectId))
+                    {
+                        return "Error";
+                    }
+                    else if (setObjectiveIdService01.Any(input.ObjectId.Contains))
+                    {
+                        if (input.ObjectId == "01")
+                        {
+                            return "Success";
+                        }
+                        else if (input.ObjectId == "02")
+                        {
+                            return "Success";
+                        }
+                        return "Success";
+                    }
+                    return "Error";
+                }
+                else if (input.ServiceId == "02")
+                {
+                    if (string.IsNullOrEmpty(input.ObjectId))
+                    {
+                        return "Error";
+                    }
+                    else if (setObjectiveIdService02.Any(input.ObjectId.Contains))
+                    {
+                        return "Success";
+                    }
+                    return "Error";
+                }
+                return "Error";
+            }
+            else
+            {
+                return "Error";
+            }
+
         }
     }
 }
